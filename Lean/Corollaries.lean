44b56d5a7187497797bbe37c7ecba877736cc167
Cgcharles7 <cgcharles7@gmail.com>
2026-06-23 23:52:20 -0400

    Update Corollaries.lean

--- Lean/Corollaries.lean
+++ Lean/Corollaries.lean
@@ -1,4 +1,4 @@
--- Defining the state of layer k+1 using your 4 corollaries
+-- Defining the state of layer k+1 for 4 corollaries 
 variable (h_decay : (bfsLayerVerts G v₀ (k + 1)).card = delta G - k)
 variable (h_regular : ∀ v ∈ bfsLayerVerts G v₀ (k + 1), (interiorNeighbors (k + 1) k u_k v).card = k)
 variable (h_no_extra : totalInteriorArcs G v₀ (k + 1) = (bfsLayerVerts G v₀ (k + 1)).card * k)
