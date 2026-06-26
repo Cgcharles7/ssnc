b3d30d4eafe18e94a33edae5d7162dd2056938a5
Cgcharles7 <cgcharles7@gmail.com>
2026-06-25 22:27:20 -0400

    Update defs.lean

--- Lean/defs.lean
+++ Lean/defs.lean
@@ -2,3 +2,7 @@ structure OrientedGraph (V : Type*) where
   Adj : V → V → Prop
   irreflexive : ∀ u : V, ¬ Adj u u
   asymmetric : ∀ {u v : V}, Adj u v ¬¬ Adj v u
+
+indictive MyVertices
+  | A | B | C
+  deriving DecidableEq
